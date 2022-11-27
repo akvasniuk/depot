96b136aad25113b9383127b29422eeeea9e46ca0
akvasnuk1 <kvasnukandriy11@gmail.com>
2022-11-27 12:25:58 +0200

    depot

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -28,7 +28,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_index_url }
+        format.js {@current_item = @line_item}
         format.json { render :show, status: :created, location: @line_item }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -55,7 +56,7 @@ class LineItemsController < ApplicationController
     @line_item.destroy
 
     respond_to do |format|
-      format.html { redirect_to line_items_url, notice: "Line item was successfully destroyed." }
+      format.html { redirect_to store_index_url }
       format.json { head :no_content }
     end
   end
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,4 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import "@fortawesome/fontawesome-free/js/all"
+import "channels"
