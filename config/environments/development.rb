f3a4062d86cf853dcdc16d30c506f28f2a11697f
akvasnuk1 <kvasnukandriy11@gmail.com>
2022-12-07 16:39:51 +0200

    depot

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,11 +72,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  ## Sending mail configuration
-  config.action_mailer.delivery_method = :smtp
-  host = 'vegetables-shop'
-  config.action_mailer.default_url_options = { host: host }
-
   config.action_mailer.smtp_settings = {
     address:    "smtp.gmail.com",
     port:       587,
