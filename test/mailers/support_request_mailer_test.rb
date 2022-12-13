a72e62148e4fd222be7275927f3269a606edbfdc
akvasnuk1 <kvasnukandriy11@gmail.com>
2022-12-13 13:56:21 +0200

    depot

--- test/mailers/support_request_mailer_test.rb
+++ test/mailers/support_request_mailer_test.rb
@@ -4,7 +4,7 @@ class SupportRequestMailerTest < ActionMailer::TestCase
   test "respond" do
     mail = SupportRequestMailer.respond
     assert_equal "Respond", mail.subject
-    assert_equal ["yorkgood5@gmail.com@example.org"], mail.to
+    assert_equal ["yorkgood5r@gmail.com@example.org"], mail.to
     assert_equal ["yorkgood4@gmail.com"], mail.from
     assert_match "Hi", mail.body.encoded
   end
