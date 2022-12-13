ac0c92ac97848972a48fc243ed599ad3f07ea6d2
akvasnuk1 <kvasnukandriy11@gmail.com>
2022-12-13 12:30:43 +0200

    depot

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -36,6 +36,7 @@ class OrdersController < ApplicationController
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
         ChargeOrderJob.perform_later(@order, pay_type_params.to_h)
+        OrderMailer.shipped(@order).deliver_later
 
         format.html { redirect_to store_index_url(locale: I18n.locale),
                                   notice: I18n.t('.thanks') }
